a1dd28f18556ae25f9e0d6e422d9e6620b22dd23
ZubaidurBagmar99 <zubaidur99@gmail.com>
2023-09-15 02:36:04 +0600

    full project commit

---
+++ src/pages/Shared/Terms/Terms.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+const Terms = () => {
+    return (
+        <div>
+            <h2>Terms and Condition</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio magnam distinctio eaque quas nisi nulla possimus ipsa natus eum, fugit cumque perspiciatis cupiditate repellat! Iure minima eaque amet laudantium explicabo, ad iste molestiae dolores doloremque, quod labore adipisci, repudiandae soluta porro. Rerum ut blanditiis accusantium atque temporibus enim assumenda soluta!</p>
+            <p>Go back to <Link to="/register">Register</Link></p>
+        </div>
+    );
+};
+
+export default Terms;
